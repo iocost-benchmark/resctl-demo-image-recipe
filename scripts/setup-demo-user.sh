1747e092ccdd5680ba36f5b91e743d131210ca6d
Christopher Obbard <chris.obbard@collabora.com>
2020-06-24 14:51:31 +0100

    setup-demo-user: add /usr/sbin to /usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/home/obbardc/go/bin
    Signed-off-by: Christopher Obbard <chris.obbard@collabora.com>

--- scripts/setup-demo-user.sh
+++ scripts/setup-demo-user.sh
@@ -5,3 +5,10 @@ PASS=${USER}
 adduser --gecos ${USER} --disabled-password --shell /bin/bash ${USER}
 adduser ${USER} sudo
 echo "${USER}:${PASS}" | chpasswd
+
+# add /usr/sbin to path
+cat <<EOF >> /home/${USER}/.profile
+
+# resctl-demo
+PATH="/usr/sbin:$PATH"
+EOF
