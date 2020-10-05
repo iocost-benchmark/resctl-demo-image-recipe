172a7d96e9232724e125e58d401e1d00fa05d0dc
Christopher Obbard <chris.obbard@collabora.com>
2020-10-05 20:43:03 +0100

    ospack: install oomd from source
    The customer has requested to install a specific version of oomd from
    source rather than using the version in the Debian archives.
    
    Signed-off-by: Christopher Obbard <chris.obbard@collabora.com>

---
+++ scripts/install-oomd.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -e
+
+GIT_TAG="v0.3.0"
+
+echo "Installing OOMD from source"
+cd /opt
+
+# install build deps
+apt-get install --yes git meson build-essential libjsoncpp-dev libsystemd-dev libgtest-dev libgmock-dev
+
+# setup git configuration
+git config --global advice.detachedHead false
+git config --global user.email "ci@collabora.com"
+git config --global user.name "Collabora CI"
+
+# clone source
+git clone https://github.com/facebookincubator/oomd.git
+cd oomd
+git checkout ${GIT_TAG}
+
+# pick relevent commits
+git cherry-pick 64e46deb6ab01900944d618a26c00a70ee0dba5e
+
+# build
+meson build && ninja -C build
+
+# install
+cd build && ninja install
+
+# cleanup
+cd / && rm -rf /opt/oomd
