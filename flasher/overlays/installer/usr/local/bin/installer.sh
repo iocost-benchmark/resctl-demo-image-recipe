iocost-benchmark/resctl-demo-image-recipe

commit 0369b260aec70201193d410325d6e0ca43ae2d9c

flasher: Attempt to regenerate btrfs fsid
Signed-off-by: Christopher Obbard <chris.obbard@collabora.com>

flasher/overlays/installer/usr/local/bin/installer.sh
@@ -49,6 +49,9 @@ echo "Installing resctl-demo to ${CHOICE}; do not turn your computer off."
 echo "You will be prompted to restart your computer after installation."
 echo ""
 
+# Discard used blocks on EMMC
+blkdiscard ${CHOICE}
+
 bmaptool \
   copy \
   ${FLASHER_STORAGE_MNT}/resctl-demo-image.img.gz \
@@ -67,6 +70,9 @@ fi
 # Fix GPT to take the full size
 echo fix | parted ---pretend-input-tty ${CHOICE} print
 
+# Reload partition table
+udevadm trigger --settle "${CHOICE}"
+
 # Expand rootfs partition table
 ROOTFS_PART_NO=2
 ROOTFS_PART=$(lsblk -n -o PATH | grep "${CHOICE}" | grep -Fvx "${CHOICE}" | sed -n "${ROOTFS_PART_NO}p")
@@ -74,7 +80,10 @@ echo "Expanding ${ROOTFS_PART}"
 parted -s ${CHOICE} resizepart ${ROOTFS_PART_NO} 100%
 
 # Reload partition table
-udevadm  trigger --settle "${CHOICE}"
+udevadm trigger --settle "${CHOICE}"
+
+# Regenerate btrfs fsid
+btrfstune -m ${ROOTFS_PART}
 
 # Expand rootfs
 ROOTFS_MNT="/mnt/rootfs"

commit ff7384aa0940e3fffcde785e5fa82e6b0e287492

flasher: Modify rootfs to find partitions by fsuuid
In previous commit we changed the flasher to regenerate the rootfs
UUID after copying the image, but the grub configuration still
attempted to find the partition by the original UUID. Fix the grub
config to use the new UUID.

While we are here, be explicit with the partition UUID in the
fstab.

Fixes: 0369b260aec7 ("flasher: Attempt to regenerate btrfs fsid")

Signed-off-by: Christopher Obbard <chris.obbard@collabora.com>

flasher/overlays/installer/usr/local/bin/installer.sh
@@ -71,7 +71,7 @@ fi
 echo fix | parted ---pretend-input-tty ${CHOICE} print
 
 # Reload partition table
-udevadm trigger --settle "${CHOICE}"
+partprobe "${CHOICE}"
 
 # Expand rootfs partition table
 ROOTFS_PART_NO=2
@@ -80,15 +80,37 @@ echo "Expanding ${ROOTFS_PART}"
 parted -s ${CHOICE} resizepart ${ROOTFS_PART_NO} 100%
 
 # Reload partition table
-udevadm trigger --settle "${CHOICE}"
+partprobe "${CHOICE}"
 
 # Regenerate btrfs fsid
+ROOTFS_BLKID_OLD=$(blkid -s UUID -o value ${ROOTFS_PART})
 btrfstune -m ${ROOTFS_PART}
 
-# Expand rootfs
+# Reload partition table
+partprobe "${CHOICE}"
+
+# Get the new fsid
+ROOTFS_BLKID_NEW=$(blkid -s UUID -o value ${ROOTFS_PART})
+
+# Mount rootfs
 ROOTFS_MNT="/mnt/rootfs"
 mkdir -p ${ROOTFS_MNT}
 mount ${ROOTFS_PART} ${ROOTFS_MNT}
+
+# Update fsid in grub configuration
+sed -i "s/$ROOTFS_BLKID_OLD/$ROOTFS_BLKID_NEW/g" "$ROOTFS_MNT/boot/grub/grub.cfg"
+
+# Update fsid in /etc/default/grub
+sed -i "s/LABEL=system/UUID=$ROOTFS_BLKID_NEW/g" "$ROOTFS_MNT/etc/default/grub"
+
+# Update fsid in /etc/fstab
+sed -i "s/LABEL=system/UUID=$ROOTFS_BLKID_NEW/g" "$ROOTFS_MNT/etc/fstab"
+
+# Fix /etc/kernel/cmdline
+sed -i "s/UUID=$ROOTFS_BLKID_OLD/UUID=$ROOTFS_BLKID_NEW/g" "$ROOTFS_MNT/etc/kernel/cmdline"
+sed -i "s/LABEL=system//g" "$ROOTFS_MNT/etc/kernel/cmdline"
+
+# Expand rootfs
 btrfs filesystem resize max ${ROOTFS_MNT}
 
 # Install lockfile inside rootfs to disable pivot on first boot