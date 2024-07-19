f202cf2f094a39a010db7a066017b225a005dc52
Christopher Obbard <chris.obbard@collabora.com>
2024-07-19 11:10:53 +0100

    qemu-flasher: Add a disk with 4096-byte sectors
    As per https://github.com/iocost-benchmark/resctl-demo-image-recipe/issues/35#issuecomment-2238736902
    we think that the root cause for the installer not working on certain hardware
    is due to modern SSDs having different sector sizes. Add a disk with fixed
    sector size to the qemu script to be able to test this case.
    
    Signed-off-by: Christopher Obbard <chris.obbard@collabora.com>

--- start-qemu-flasher-efi.sh
+++ start-qemu-flasher-efi.sh
@@ -3,10 +3,12 @@
 FLASHER_IMG="resctl-demo-flasher-efiboot.img"
 ROOT1_IMG="resctl-demo-flasher-efiboot-testroot1.img"
 ROOT2_IMG="resctl-demo-flasher-efiboot-testroot2.img"
+ROOT3_IMG="resctl-demo-flasher-efiboot-testroot3.img"
 
 # create a disk to install to
 qemu-img create -f qcow2 ${ROOT1_IMG} 512G
 qemu-img create -f qcow2 ${ROOT2_IMG} 512G
+qemu-img create -f qcow2 ${ROOT3_IMG} 512G
 
 qemu-system-x86_64 \
   -machine type=q35,accel=kvm \
@@ -19,6 +21,7 @@ qemu-system-x86_64 \
   -device virtio-scsi-pci,id=scsi1 \
   -drive file=${ROOT1_IMG},if=none,format=qcow2,discard=unmap,aio=native,cache=none,id=testroot1 -device scsi-hd,drive=testroot1,bus=scsi1.0 \
   -drive file=${ROOT2_IMG},if=none,format=qcow2,discard=unmap,aio=native,cache=none,id=testroot2 -device nvme,drive=testroot2,serial=1234 \
+  -drive file=${ROOT3_IMG},if=none,format=qcow2,discard=unmap,aio=native,cache=none,id=testroot3 -device virtio-blk,drive=testroot3,serial=4096_blk_size,logical_block_size=4096,physical_block_size=4096 \
   -boot menu=on \
   -vga qxl \
   -serial stdio \
